4450ed9e904c9b79efa8615164d127fa7caa7511
AbdrahmanovIM <i.abdarahmanov@modulbank.ru>
2021-11-14 20:44:22 +0500

    -

---
+++ evaluate_for_negative.py
@@ -0,0 +1,31 @@
+
+
+from affinn_ru import AFFINN
+
+
+def split_text(msg_text):
+    words = msg_text.split()
+    return words
+
+
+def evaluate_word(word):
+    _word = word.lower()
+    for key, value in AFFINN.items():
+        if key in _word:
+            return int(float(value))
+    return 0
+
+
+def evaluate_text(msg_text):
+    tokens = split_text(msg_text)
+    text_rate = 0
+    for _word in tokens:
+        word_rate = evaluate_word(_word)
+        text_rate += word_rate
+    return text_rate
+
+
+if __name__ == '__main__':
+    text = """Ну просто пизда там ещё как было - давали задачи на распределение, все «сложные» в понимании 
+    ТЗ отдавали более опытным тестировщикам, потому что там реально надо было «выбивать» требования )))"""
+    print(evaluate_text(text))
--- replacing.py
+++ replacing.py
@@ -3,27 +3,30 @@
 from replaces import REPLACES
 
 
-def split_text(text):
-    words = text.split()
+def split_text(msg_text):
+    words = msg_text.split()
     return words
 
 
 def replace_word(word):
     is_title = word[0].isupper()
-    word = word.lower()
+    _word = word.lower()
     for key, value in REPLACES.items():
-        if key in word:
-            result_word = word.replace(key, value)
+        if key in _word:
+            result_word = _word.replace(key, value)
             return result_word.title() if is_title else result_word
     return word
 
 
-
-if __name__ == '__main__':
-    text = """Мама прочти сообщение. Помоги сделать работу. 
-    Как классно писать бота в воскресенье вечером"""
-    tokens = split_text(text)
+def replace_text(msg_text):
+    tokens = split_text(msg_text)
     print(tokens)
     replaced_word = [replace_word(word) for word in tokens if word is not None]
-    result = ' '.join(replaced_word)
-    print(result)
+    return ' '.join(replaced_word)
+
+
+
+if __name__ == '__main__':
+    text = """Ну просто пизда хуйня там ещё как было - давали задачи на распределение, все «сложные» в понимании 
+    ТЗ отдавали более опытным тестировщикам, потому что там реально надо было «выбивать» требования )))"""
+    print(replace_text(text))
