d1e20d149dc3049108d681073c81eb193df7198d
AbdrahmanovIM <i.abdarahmanov@modulbank.ru>
2022-08-10 17:46:02 +0500

    fix

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_PATH = f'/webhook/{os.getenv("NEW_RELIC_LICENSE_KEY")}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
