5706e9fe8502ede126bfda97efd306ec0fcead4e
AbdrahmanovIM <i.abdarahmanov@modulbank.ru>
2022-08-11 12:28:05 +0500

    fix

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def replace_message(message: types.Message):
     if is_command_to_reply and message.reply_to_message is not None:
         reply_to_message = message.reply_to_message.text
         output_msg = replace_text(reply_to_message)
-        await message.reply(output_msg)
+        await message.reply_to_message.reply(output_msg)
     if is_negative or is_ref_bot or random.randint(1, 100) < 10:
         output_msg = replace_text(input_msg)
         await message.reply(output_msg)
